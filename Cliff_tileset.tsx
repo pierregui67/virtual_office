acf5c4fe23cd75fe650654244458ad18dc35c270
pierre <pierre.gui@wanadoo.fr>
2021-01-23 16:53:49 +0100

    add tilesets

---
+++ Cliff_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Cliff_tileset" tilewidth="32" tileheight="32" tilecount="240" columns="15">
+ <image source="../../Users/dev/Downloads/Cliff_tileset.png" width="503" height="518"/>
+</tileset>
