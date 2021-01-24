71c895130b927d40120c7be6cc912d610e10b79a
pierre <pierre.gui@wanadoo.fr>
2021-01-24 20:16:51 +0100

    add new building and tilesets

--- tree side.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2021.01.13" name="tree side" tilewidth="32" tileheight="32" tilecount="418" columns="19">
- <image source="../../Users/dev/Downloads/_tree_01/_tree_01/_tree_01_10000.png" width="631" height="723"/>
-</tileset>
